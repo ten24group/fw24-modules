6c8508219b81ac5f74b04bbde046f6da36de0cab
Nitin Yadav <nitinrajyadav@outlook.com>
2024-10-25 17:58:44 +0530

    Add: fw24 package for rendering pdfs

---
+++ packages/pdf-renderer/src/index.ts
@@ -0,0 +1,40 @@
+import { AbstractFw24Module, FW24Construct, createLogger, ILogger, DIModule, LayerConstruct } from '@ten24group/fw24';
+import { PDFRendererService } from './pdf-renderer-service';
+
+interface IPDFRendererModuleConfig {
+}
+
+@DIModule({
+    exports: [PDFRendererService]
+})
+class PDFRendererModule extends AbstractFw24Module {
+    readonly logger: ILogger = createLogger(PDFRendererModule);
+    protected constructs: Map<string, FW24Construct>; 
+
+    constructor( protected readonly config: IPDFRendererModuleConfig){
+        super(config);
+        this.constructs = new Map();
+
+        const chromiumLayer = new LayerConstruct([{
+            layerName: 'pdf_renderer_module_layer',
+            sourcePath: 'node_modules/@sparticuz/',
+            mode: 'PACKAGE_DIRECTORY',
+        }]);
+
+        this.constructs.set('pdf_renderer_module_layer', chromiumLayer );
+    }
+
+    getBasePath(): string {
+        return __dirname;
+    }
+
+    getConstructs(): Map<string, FW24Construct> {
+        return this.constructs;
+    }
+}
+
+export { 
+    PDFRendererModule, 
+    PDFRendererService,
+    IPDFRendererModuleConfig, 
+};
