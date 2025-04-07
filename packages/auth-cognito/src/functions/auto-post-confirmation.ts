701f64ed8712884515058f60e16b641528bd8b66
Sumit Verma <sumit.verma@ten24web.com>
2025-04-07 11:33:11 -0400

    only trigger on signup:

--- packages/auth-cognito/src/functions/auto-post-confirmation.ts
+++ packages/auth-cognito/src/functions/auto-post-confirmation.ts
@@ -2,7 +2,11 @@ import { AdminAddUserToGroupCommand, CognitoIdentityProviderClient } from '@aws-
 import { resolveEnvValueFor } from '@ten24group/fw24';
 
 export const handler = async (event: any) => {
-    console.log('::handler:: Event: ', event);
+    // Only assign the default role if this is a new sign-up confirmation.
+    if (event.triggerSource !== 'PostConfirmation_ConfirmSignUp') {
+        return event;
+    }
+
     const username = event.userName;
     const userPoolID = event.userPoolId;
     const autoUserSignupGroups = getAutoUserSignupGroups().split(',');
